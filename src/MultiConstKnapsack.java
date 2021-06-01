38afb913bdfd13d7667f0eef637573842b28ecbd
yasin <yasin.142.tarakci@gmail.com>
2021-06-01 20:17:12 +0300

    readFile method created.

--- src/MultiConstKnapsack.java
+++ src/MultiConstKnapsack.java
@@ -3,16 +3,35 @@ import java.io.File;
 import java.io.FileNotFoundException;
 
 public class MultiConstKnapsack {
+    static Item[] items;
+    static int[] capacities;
 
     public static void main(String[] args) throws FileNotFoundException {
 
-        File inputFile = new File("sampleinput.txt");
+        readFile("sample1.txt");
+        for (int i = 0; i < capacities.length; i++) {
+            for (Item item : items) System.out.println(item.toString());
+        }
+
+//        readFile("sample2.txt");
+//        for (int i = 0; i < capacities.length; i++) {
+//            for (Item item : items) System.out.println(item.toString());
+//        }
+//
+//        readFile("sample3.txt");
+//        for (int i = 0; i < capacities.length; i++) {
+//            for (Item item : items) System.out.println(item.toString());
+//        }
+    }
+
+    private static void readFile(String fileName) throws FileNotFoundException {
+        File inputFile = new File(fileName);
         Scanner sc = new Scanner(inputFile);
 
         int knapCount = sc.nextInt();
         int itemCount = sc.nextInt();
-        Item[] items = new Item[itemCount];
-        int[] capacities = new int[knapCount];
+        items = new Item[itemCount];
+        capacities = new int[knapCount];
         for (int i = 0; i < itemCount; i++) {
             int value = sc.nextInt();
             Item currentItem = new Item(value, knapCount);
